47c7eb7ad2f80506e5ddc86f112f06c4b89a86c8
peterlesko <peterlesko@yahoo.com>
2020-05-17 13:26:58 +0100

    Chapter 13 using WebClient for API calls

--- movie-catalog-service/src/main/java/com/plesk/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/plesk/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +21,9 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogueItem> getCatalog(@PathVariable ("userId") String userId) {
 
@@ -29,10 +34,19 @@ public class MovieCatalogResource {
         );
 
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class );
+           // Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class );
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+
             return new CatalogueItem(movie.getName(), "Des", rating.getRating());
         })
         .collect(Collectors.toList());
 
+        // For each movie ID, call info service and get details
+        // Put them all together
     }
 }
